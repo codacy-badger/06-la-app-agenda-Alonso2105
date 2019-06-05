0c5e5d0c7f9c44f8dfff843edc1f9c97eb2e357d
20155815 ALONSO LOPEZ MALDONADO <20155815@TELEMATICA.UCOL.MX>
2019-06-05 14:17:52 -0500

    coreccion errores

---
+++ js/Agenda.js
@@ -0,0 +1,86 @@
+import VistaWeb from "./VistaWeb.js";
+
+var vistaTabla = new VistaWeb(document.querySelector("#informacion"));
+
+export default class Agenda {
+    constructor() {
+
+        this._contactos = [];
+        this._actualizacion();
+    }
+    _actualizacion() {
+        let mContactos = JSON.parse(localStorage.getItem("contactos"));
+        if (mContactos === null) {
+            return;
+        }
+        mContactos.forEach((contacto, index) => {
+            this._contactos.push(contacto);
+        });
+    }
+    _eliminacion(row, contacto) {
+        Swal.fire({
+            type: "question",
+            title: "¿Deseas eliminar al contacto?",
+            text: contacto.nombre,
+            showCancelButton: true,
+            confirmButtonText: "Sí",
+            cancelButtonText: "No"
+        }).then(result => {
+            if (result.value) {
+                let pos = this._buscarPersona(contacto.correo);
+                this._contactos.splice(pos, 1);
+
+                localStorage.setItem("contactos", JSON.stringify(this._contactos));
+                row.remove();
+            }
+        });
+    }
+    _buscarPersona(correo) {
+        let result = -1;
+        this._contactos.forEach((contacto, index) => {
+            if (contacto.correo === correo) {
+                result = index;
+                return;
+            }
+        });
+        return result;
+    }
+    _ordenarPersonas(){
+        function comparar(x,z){
+            if ( x.nombre < z.nombre ){
+                return -1;
+              }
+              if ( x.nombre > z.nombre){
+                return 1;
+              }
+              return 0
+        }
+        this._contactos.sort(comparar);
+    }
+    añadirPersona(contacto) {
+        if (this._buscarPersona(contacto.correo) >= 0) {
+            Swal.fire({
+                type: "error",
+                title: "Error",
+                text: `Ya existe un usario con el correo de: ${contacto.correo}`
+            });
+            return;
+        }
+        let objContacto = {
+            nombre: contacto.nombre,
+            correo: contacto.correo,
+            fechaNacimiento: contacto.fechaNacimiento,
+        };
+        this._contactos.push(objContacto);
+        this._ordenarPersonas();
+        vistaTabla.enseñar(this._contactos);
+
+
+        localStorage.setItem("contactos", JSON.stringify(this._contactos));
+        Swal.fire({
+            type: "success",
+            title: "Correcto",
+            text: `El usuario se ha registrado correctamente con el correo de: ${contacto.correo}`
+        });
+    }    
+}
---
+++ js/Contacto.js
@@ -0,0 +1,44 @@
+export default class Contacto{
+    constructor(contacto){
+        this._nombre = contacto.nombre.toUpperCase();
+        this._correo = contacto.correo;
+        this._fechaNacimiento = new Date(contacto.fechaNacimiento);
+        this._meses = [
+            "Ene",
+            "Feb",
+            "Mar",
+            "Abr",
+            "May",
+            "Jun",
+            "Jul",
+            "Ago",
+            "Sept",
+            "Oct",
+            "Nov",
+            "Dic"
+        ];
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+    get correo(){
+        return this._correo;
+    }
+    get fechaNacimiento(){
+        return  this._fechaNacimiento;
+    }
+    get fechaNacimientoString(){
+        let fecha = this._fechaNacimiento.getDate() + "/" + this._meses[this._fechaNacimiento.getMonth()] + "/" + this._fechaNacimiento.getFullYear();
+        return fecha;
+    }
+
+    getEdad(){
+        let unDia = 24 * 60 * 60 * 1000;
+        let unAño = unDia * 365;
+        let diferenciaMS = new Date() - this._fechaNacimiento;
+        let edad = Math.trunc(diferenciaMS / unAño);
+        return edad;
+    }
+
+}
